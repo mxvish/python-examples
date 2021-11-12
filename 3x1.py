0c580e9a994b5618ef8eedfda4ec66065bd39b3f
Kenta Oshima <84490675+kenteroshima@users.noreply.github.com>
2021-11-12 07:59:58 -0500

    Add files via upload

---
+++ 3x1.py
@@ -0,0 +1,49 @@
+#import sys
+#print(sys.argv[1])
+from matplotlib import pyplot as plt
+
+number=27
+count=0
+
+xaxis=[]
+yaxis=[]
+time=1
+
+while(True):
+    xaxis.append(time)
+    yaxis.append(number)
+    time+=1
+    print(number)   
+    if(number==4):
+        count+=1
+        break
+    elif(number%2==0):
+        number/=2
+    else:
+        number=number*3+1
+
+"""            
+for num in range(1, number+1):
+    while(True):
+        if(num==4):
+            count+=1
+            break
+        elif(num%2==0):
+            num/=2
+        else:
+            num=num*3+1
+
+"""
+
+print("\nfinish!\n")            
+#print(number-count)
+print(xaxis)
+print("\n")
+print(yaxis)
+
+plt.plot(xaxis,yaxis)
+plt.title("Line graph")  
+plt.ylabel('Y axis')  
+plt.xlabel('X axis')
+plt.show()
+
---
+++ plot.py
@@ -0,0 +1,16 @@
+from matplotlib import pyplot as plt
+
+xaxis=[]
+yaxis=[]
+for x in range(3):
+    xaxis.append(x+1)
+    yaxis.append(pow(x+1, 2))
+
+print(xaxis)
+print(yaxis)
+
+plt.plot(xaxis,yaxis)
+plt.title("Line graph")  
+plt.ylabel('Y axis')  
+plt.xlabel('X axis')
+plt.show()
