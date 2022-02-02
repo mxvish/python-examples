d1475704cd32c4f7b26557bb3795448db33c9971
Kenta Oshima <84490675+kenteroshima@users.noreply.github.com>
2022-02-02 14:10:22 +0900

    Update collatz_conjecture_plot.py

--- collatz_conjecture_plot.py
+++ collatz_conjecture_plot.py
@@ -1,47 +1,27 @@
 from matplotlib import pyplot as plt
+import sys
 
-number=27
-count=0
+if len(sys.argv) > 1:
+    number = int(sys.argv[1])
+else:
+    number = 27
 
-xaxis=[]
-yaxis=[]
-time=1
+startnum = number
+yaxis = []
 
-while(True):
-    xaxis.append(time)
-    yaxis.append(number)
-    time+=1
-    print(number)   
-    if(number==4):
-        count+=1
+while True:
+    yaxis.append(int(number))
+
+    if number == 1:
         break
-    elif(number%2==0):
-        number/=2
+    elif number % 2 == 0:
+        number /= 2
     else:
-        number=number*3+1
+        number = number * 3 + 1
 
-"""            
-for num in range(1, number+1):
-    while(True):
-        if(num==4):
-            count+=1
-            break
-        elif(num%2==0):
-            num/=2
-        else:
-            num=num*3+1
-
-"""
-
-print("\nfinish!\n")            
-#print(number-count)
-print(xaxis)
-print("\n")
+print("finish!")            
 print(yaxis)
 
-plt.plot(xaxis,yaxis)
-plt.title("Line graph")  
-plt.ylabel('Y axis')  
-plt.xlabel('X axis')
+plt.plot(yaxis)
+plt.title("Collatz conjecture graph starts with " + str(startnum))  
 plt.show()
-
