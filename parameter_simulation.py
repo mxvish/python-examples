df7a7d91563c8e6da0bfffb7f1f0ef5bd39edf4e
Kenta Oshima <84490675+kenteroshima@users.noreply.github.com>
2022-02-04 14:37:40 +0900

    Update parameter_simulation.py

--- parameter_simulation.py
+++ parameter_simulation.py
@@ -1,58 +1,57 @@
 import random as rd
 
 def demo(player_hp, player_attack1, player_attack2):
-	win=0
-	turn=0
+	win,turn = 0, 0
 
 	for x in range(10000):
-		hp_1=player_hp
-		hp_2=player_hp
-		king_hp=6500
+		hp_1 = player_hp
+		hp_2 = player_hp
+		king_hp = 6500
 
 		while hp_1>0 or hp_2>0 and king_hp>0:
-			turn+=1
+			turn += 1
 			if hp_1>0:
-				king_hp-=rd.randint(1,6)*player_attack1
+				king_hp -= rd.randint(1,6) * player_attack1
 			if hp_2>0:
-				king_hp-=rd.randint(1,6)*player_attack2
+				king_hp -= rd.randint(1,6) * player_attack2
 
 			if king_hp<=0:
-				win+=1
+				win += 1
 				break
 
-			damage=rd.randint(0,4)*70
-			randnum=rd.randint(1,12)
+			damage = rd.randint(0,4) * 70
+			randnum = rd.randint(1,12)
 
 			for x in range(2):
 				if 1<=randnum and randnum<=4:
 					if hp_1>0:
-						hp_1-=damage
+						hp_1 -= damage
 					else:
-						hp_2-=damage
+						hp_2 -= damage
 					if hp_2>0:
-						hp_2-=damage
+						hp_2 -= damage
 					else:
-						hp_1-=damage
+						hp_1 -= damage
 				elif 5<=randnum and randnum<=7:
 					if hp_1>0:
-						hp_1-=damage
+						hp_1 -= damage
 					else:
-						hp_2-=damage
+						hp_2 -= damage
 				elif 8<=randnum and randnum<=10:
 					if hp_2>0:
-						hp_2-=damage
+						hp_2 -= damage
 					else:
-						hp_1-=damage
+						hp_1 -= damage
 				else:
-					king_hp+=1000
+					king_hp += 1000
 
 			if hp_1<=0 and hp_2<=0:
 						break
 
 	print("--------------------------------------------------------")
-	print("player_hp	",player_hp)
-	print("player_attack1",player_attack1)
-	print("player_attack2",player_attack2)
+	print("player_hp	", player_hp)
+	print("player_attack1", player_attack1)
+	print("player_attack2", player_attack2)
 	print(win/10000)
 	print(turn/10000)
 
