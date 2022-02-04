0c137a3894ada5c2c2ab136103911c1659b8896f
Kenta Oshima <84490675+kenteroshima@users.noreply.github.com>
2022-02-04 14:34:23 +0900

    make this code easier. to read

--- class_for_short_runtime.py
+++ class_for_short_runtime.py
@@ -1,12 +1,11 @@
 import numpy.random as rd
 
 for x in range(1000000):
-    a=rd.randint(2)#took 2.94 seconds to execute
+    a = rd.randint(2)   #took 2.94 seconds to execute
 
 """
 import numpy as np
 
 for x in range(1000000):
-    a=np.random.randint(2)#took 3.00 seconds to execute
-
+    a = np.random.randint(2)    #took 3.00 seconds to execute
 """
