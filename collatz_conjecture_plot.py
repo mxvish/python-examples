5f0470fc4831b3bda52a4f5b408a589bb9f28287
Kenta Oshima <84490675+kenteroshima@users.noreply.github.com>
2022-02-05 20:04:21 +0900

    enable to enter number from gui

--- collatz_conjecture_plot.py
+++ collatz_conjecture_plot.py
@@ -1,27 +1,46 @@
 from matplotlib import pyplot as plt
-import sys
+import tkinter as tk      
+from functools import partial
 
-if len(sys.argv) > 1:
-    number = int(sys.argv[1])
-else:
-    number = 7
+def call_result(label_result, arg):
+    try:
+        number = int(arg.get())
+        startNum = number
+        yaxis = []
+        
+        while number != 1:
+            yaxis.append(number)
+        
+            if number % 2 == 0:
+                number /= 2
+            else:
+                number = number * 3 + 1
+        
+        yaxis.append(number)
+        print("finish!")            
+        print(yaxis)
+        
+        plt.plot(yaxis)
+        plt.title("Collatz conjecture graph starts with " + str(startNum))  
+        plt.show()
+        return
 
-startnum = number
-yaxis = []
+    except ValueError:
+        label_result.config(text="Please enter number") 
 
-while True:
-    yaxis.append(int(number))
+root = tk.Tk()                                                                  
+root.geometry('480x270+100+100')  
+root.title('Collatz conjecture plot')  
+   
+inputStr = tk.StringVar()  
+  
+labelNum = tk.Label(root, text="number").grid(row=1, column=0)  
+labelResult = tk.Label(root)
+labelResult.grid(row=7, column=2)  
 
-    if number == 1:
-        break
-    elif number % 2 == 0:
-        number /= 2
-    else:
-        number = number * 3 + 1
-
-print("finish!")            
-print(yaxis)
-
-plt.plot(yaxis)
-plt.title("Collatz conjecture graph starts with " + str(startnum))  
-plt.show()
+entry = tk.Entry(root, textvariable=inputStr).grid(row=1, column=2)  
+  
+call_result = partial(call_result, labelResult, inputStr)  
+buttonPlot = tk.Button(root, text="plot", command=call_result).grid(row=3, column=0)  
+  
+root.mainloop()  
